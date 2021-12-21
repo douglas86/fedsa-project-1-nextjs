c9fa35e476c0b0a4b70a901e0395e361fccd1e5a
douglas86 <douglasmaxton@gmail.com>
2021-12-21 19:06:05 +0200

    Created a Card comp and style sheet

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,13 @@
-import Banner from '../components/Banner';
 import styles from '../public/static/css/pages/index.module.css';
+
+import Banner from '../components/Banner';
+import Card from '../components/Card';
 
 const Home = () => {
     return (
         <>
             <Banner />
-            <div className={styles.card}>
-                <h1>This is a card</h1>
-            </div>
+            <Card />
         </>
     );
 };
