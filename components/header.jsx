e417865b977ac5ed91d851e31e8ca1ddcd0add17
douglas86 <douglasmaxton@gmail.com>
2021-12-21 13:49:05 +0200

    Aligned all items in list next to one another

--- components/header.jsx
+++ components/header.jsx
@@ -9,9 +9,22 @@ const Header = () => {
                 <Image src={logo} alt="logo" width={100} height={80} />
             </div>
             <div className={styles.links}>
-                <p className={styles.link}>FEATURE</p>
-                <p className={styles.link}>ABOUT</p>
-                <p className={styles.link}>CONTACT</p>
+                <ul></ul>
+                <li className={styles.links}>
+                    <a className={styles.link} href="#">
+                        FEATURE
+                    </a>
+                </li>
+                <li className={styles.links}>
+                    <a className={styles.link} href="#">
+                        ABOUT
+                    </a>
+                </li>
+                <li className={styles.links}>
+                    <a className={styles.link} href="#">
+                        CONTACT
+                    </a>
+                </li>
             </div>
         </div>
     );
