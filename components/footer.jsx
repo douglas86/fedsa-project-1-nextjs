21c69be11854ed9140357a3842c67bad2c9ed22e
douglas86 <douglasmaxton@gmail.com>
2021-12-21 16:15:20 +0200

    Finished styling for desktop with flex

--- components/footer.jsx
+++ components/footer.jsx
@@ -5,9 +5,35 @@ import Image from 'next/image';
 const Footer = () => {
     return (
         <div className={styles.footer}>
-            <h3>This is the footer</h3>
-            <div>
-                <Image src={logo} alt="logo" width={50} height={20} />
+            <div className={styles.content}>
+                <div className={styles.leftColumn}>
+                    <h3>RICCA</h3>
+                    <ul className={styles.links}>
+                        <li className={styles.link}>Our Story</li>
+                        <li className={styles.link}>Our Green Initiative</li>
+                        <li className={styles.link}>Store Locator</li>
+                        <li className={styles.link}>Careers</li>
+                    </ul>
+                </div>
+                <div className={styles.centerColumn}>
+                    <h3>ACCOUNT</h3>
+                    <ul className={styles.links}>
+                        <li className={styles.link}>Your Cart</li>
+                        <li className={styles.link}>Your Wishlist</li>
+                        <li className={styles.link}>Orders</li>
+                    </ul>
+                </div>
+                <div className={styles.rightColumn}>
+                    <h3>SERVICES</h3>
+                    <ul className={styles.links}>
+                        <li className={styles.link}>Terms & Conditions</li>
+                        <li className={styles.link}>Privacy Policy</li>
+                        <li className={styles.link}>FAQ</li>
+                    </ul>
+                </div>
+            </div>
+            <div className={styles.logo}>
+                <Image src={logo} alt="logo" width={100} height={15} />
             </div>
         </div>
     );
