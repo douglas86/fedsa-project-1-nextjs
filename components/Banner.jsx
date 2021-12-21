6221cce7ed7fb97e541b6eed876af654c46088ea
douglas86 <douglasmaxton@gmail.com>
2021-12-21 17:14:14 +0200

    Created text inside of a solid white border

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -3,7 +3,11 @@ import styles from '../public/static/css/components/Banner.module.css';
 const Banner = () => {
     return (
         <div className={styles.banner}>
-            <h3>This is the Banner component</h3>
+            <div className={styles.banner_text}>
+                <h1>Winter Sale</h1>
+                <h1>35% off</h1>
+                <button className={styles.button}>SEE DEALS</button>
+            </div>
         </div>
     );
 };
