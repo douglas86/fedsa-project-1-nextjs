3c8039c6701541610aa9d5d5962fd938c5d1f26c
douglas86 <douglasmaxton@gmail.com>
2021-12-21 15:20:59 +0200

    Created a footer component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,12 @@
 import Header from './header';
+import Footer from './footer';
 
 const Layout = ({ children }) => {
     return (
         <>
             <Header />
             <main>{children}</main>
+            <Footer />
         </>
     );
 };
---
+++ components/footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <>
+            <h3>This is the footer</h3>
+        </>
+    );
+};
+
+export default Footer;
