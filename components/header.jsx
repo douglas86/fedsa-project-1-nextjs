209306cd0b3aec33e1829d8133185466d7c54669
douglas86 <douglasmaxton@gmail.com>
2021-12-21 15:05:54 +0200

    Used flexbox to align items

--- components/header.jsx
+++ components/header.jsx
@@ -4,12 +4,11 @@ import Image from 'next/image';
 
 const Header = () => {
     return (
-        <div className={styles.header}>
+        <div className={styles.container}>
             <div className={styles.logo}>
                 <Image src={logo} alt="logo" width={100} height={80} />
             </div>
-            <div className={styles.links}>
-                <ul></ul>
+            <ul>
                 <li className={styles.links}>
                     <a className={styles.link} href="#">
                         FEATURE
@@ -25,7 +24,7 @@ const Header = () => {
                         CONTACT
                     </a>
                 </li>
-            </div>
+            </ul>
         </div>
     );
 };
