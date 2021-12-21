0a2432982019c0adf41d4b2effa0f336e014aa9f
douglas86 <douglasmaxton@gmail.com>
2021-12-21 15:43:25 +0200

    Added logo to footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,9 +1,14 @@
 import styles from '../public/static/css/components/footer.module.css';
+import logo from '../public/static/images/logo.svg';
+import Image from 'next/image';
 
 const Footer = () => {
     return (
         <div className={styles.footer}>
             <h3>This is the footer</h3>
+            <div>
+                <Image src={logo} alt="logo" width={50} height={20} />
+            </div>
         </div>
     );
 };
