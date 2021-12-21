ca363588ea0720d90fc75b527c35e2a2f2e58dd1
douglas86 <douglasmaxton@gmail.com>
2021-12-21 15:23:52 +0200

    Created a styling sheet for the footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,8 +1,10 @@
+import styles from '../public/static/css/components/footer.module.css';
+
 const Footer = () => {
     return (
-        <>
+        <div className={styles.footer}>
             <h3>This is the footer</h3>
-        </>
+        </div>
     );
 };
 
