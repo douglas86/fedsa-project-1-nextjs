df1e1da2bd3fdfd87ca2f701dd3ec81632d3b289
douglas86 <douglasmaxton@gmail.com>
2021-12-21 12:46:02 +0200

    Inserted a logo into the header component

--- components/header.jsx
+++ components/header.jsx
@@ -1,7 +1,13 @@
+import styles from '../public/static/css/header.module.css';
+import logo from '../public/static/images/logo.svg';
+import Image from 'next/image';
+
 const Header = () => {
     return (
         <>
-            <h1>This is the header component</h1>
+            <div className={styles.logo}>
+                <Image src={logo} alt="logo" width={100} height={80} />
+            </div>
         </>
     );
 };
