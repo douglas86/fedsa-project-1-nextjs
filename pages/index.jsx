a59e41f2e3d3521a6efd2ae3b9b325f92953c841
douglas86 <douglasmaxton@gmail.com>
2021-12-21 18:58:25 +0200

    Created a style sheet for the index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,13 @@
 import Banner from '../components/Banner';
+import styles from '../public/static/css/pages/index.module.css';
 
 const Home = () => {
     return (
         <>
             <Banner />
+            <div className={styles.card}>
+                <h1>This is a card</h1>
+            </div>
         </>
     );
 };
