4fb91406a86891b5af9bc58d2f066b527c1a7c2a
douglas86 <douglasmaxton@gmail.com>
2021-12-21 12:14:35 +0200

    Created and imported the header component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,9 @@
+import Header from '../components/header';
+
 const Home = () => {
     return (
         <>
-            <h1>This is the home page</h1>
+            <Header />
         </>
     );
 };
