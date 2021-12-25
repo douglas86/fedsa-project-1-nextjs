e2372402f324f66b2cf856004894e33f826d93b7
douglas86 <douglasmaxton@gmail.com>
2021-12-25 20:24:45 +0200

    Reset all cards just displays image of chair

--- components/Card.jsx
+++ components/Card.jsx
@@ -18,48 +18,6 @@ const Card = () => {
                                   height={500}
                               />
                           </div>
-                          <section className={styles.textContainer}>
-                              <div className={styles.textContainerTop}>
-                                  <h3>{item.title}</h3>
-                                  <p>{item.details}</p>
-                              </div>
-                              <div className={styles.textContainerBottom}>
-                                  <ul className={styles.prices}>
-                                      <li className={styles.pricesTop}>
-                                          R{item.originalPrice}
-                                      </li>
-                                      <li className={styles.pricesBottom}>
-                                          R{item.discountedPrice}
-                                      </li>
-                                  </ul>
-                                  <div className={styles.icons}>
-                                      <button
-                                          className={styles.productCardButton}
-                                      >
-                                          <div
-                                              className={styles.cardButtonHeart}
-                                          >
-                                              <Image
-                                                  src={`/static/images/assets/heart.svg`}
-                                                  alt="heart"
-                                                  width={200}
-                                                  height={200}
-                                              />
-                                          </div>
-                                      </button>
-                                      <button
-                                          className={styles.productCardButton}
-                                      >
-                                          <Image
-                                              src={`/static/images/assets/cart.svg`}
-                                              alt="cart"
-                                              width={200}
-                                              height={200}
-                                          />
-                                      </button>
-                                  </div>
-                              </div>
-                          </section>
                       </div>
                   ))
                 : null}
