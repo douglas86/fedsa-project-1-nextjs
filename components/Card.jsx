b283f99b61441c534d969ee4a844b32756006c00
douglas86 <douglasmaxton@gmail.com>
2021-12-26 15:49:20 +0200

    Can change cards based on button clicked

--- components/Card.jsx
+++ components/Card.jsx
@@ -3,61 +3,52 @@ import styles from '../public/static/css/components/Card.module.css';
 import { Getter } from './api';
 
 const Card = ({ props }) => {
-    console.log('props', props);
-
     return (
-        <div>
-            <h1>This is the card</h1>
-            {/* {props !== undefined
-                ? Object.values(props.data).map((item) => (
-                      <div key={item.ObjectId} className={styles.card}>
-                          <div className={styles.image}>
-                              <Image
-                                  src={`/static/images/chairs/${item.image}`}
-                                  alt="chair"
-                                  width={500}
-                                  height={500}
-                              />
-                          </div>
-                          <div className={styles.secondHalf}>
-                              <div className={styles.TopSection}>
-                                  <h3>{item.title}</h3>
-                                  <p>{item.details}</p>
-                              </div>
-                              <div className={styles.BottomSection}>
-                                  <div className={styles.lists}>
-                                      <ul>
-                                          <li className={styles.topPrice}>
-                                              R{item.originalPrice}
-                                          </li>
-                                          <li className={styles.bottomPrice}>
-                                              R{item.discountedPrice}
-                                          </li>
-                                      </ul>
-                                  </div>
-                                  <div className={styles.icons}>
-                                      <div className={styles.iconImage}>
-                                          <Image
-                                              src={`/static/images/assets/heart.svg`}
-                                              alt="heart"
-                                              width={50}
-                                              height={50}
-                                          />
-                                      </div>
-                                      <div className={styles.iconImage}>
-                                          <Image
-                                              src={`/static/images/assets/cart.svg`}
-                                              alt="cart"
-                                              width={50}
-                                              height={50}
-                                          />
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  ))
-                : null} */}
+        <div className={styles.card}>
+            <div className={styles.image}>
+                <Image
+                    src={`/static/images/chairs/${props.image}`}
+                    alt="chair"
+                    width={500}
+                    height={500}
+                />
+            </div>
+            <div className={styles.secondHalf}>
+                <div className={styles.TopSection}>
+                    <h3>{props.title}</h3>
+                    <p>{props.details}</p>
+                </div>
+                <div className={styles.BottomSection}>
+                    <div className={styles.lists}>
+                        <ul>
+                            <li className={styles.topPrice}>
+                                R{props.originalPrice}
+                            </li>
+                            <li className={styles.bottomPrice}>
+                                R{props.discountedPrice}
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={styles.icons}>
+                        <div className={styles.iconImage}>
+                            <Image
+                                src={`/static/images/assets/heart.svg`}
+                                alt="heart"
+                                width={50}
+                                height={50}
+                            />
+                        </div>
+                        <div className={styles.iconImage}>
+                            <Image
+                                src={`/static/images/assets/cart.svg`}
+                                alt="cart"
+                                width={50}
+                                height={50}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- pages/index.jsx
+++ pages/index.jsx
@@ -9,24 +9,6 @@ const Home = () => {
     const Get = Getter('/api/card');
     const [count, setCount] = useState(0);
 
-    // let arr = ['java', 'javascript', 'nodejs', 'php'];
-
-    // console.log('keys', arr.slice(0, 2));
-
-    // arr.slice(0, 2).forEach((item) => {
-    //     console.log('item', item);
-    // });
-
-    console.log('Button clicked: ', count);
-    console.log(
-        'type',
-        Get !== undefined
-            ? Object.entries(Get.data)
-                  .slice(count, count + 3)
-                  .map(([key, values]) => console.log('items', values))
-            : null
-    );
-
     return (
         <>
             <Banner />
@@ -35,7 +17,6 @@ const Home = () => {
                       .slice(count, count + 3)
                       .map(([key, values]) => <Card key={key} props={values} />)
                 : null}
-            {/* <Card props={Get} /> */}
             <button
                 onClick={() => (count >= 6 ? setCount(0) : setCount(count + 3))}
                 className={styles.button}
